8f7c330419b9c5bc9a6930a9c545adef529bee67
MiguelMartinez94 <122044737@upq.edu.mx>
2025-05-16 18:19:39 -0600

    Variables en Python
    Aquí conocemos algunas formas de trabajar con variables.

--- practica1/sintaxis.py
+++ practica1/sintaxis.py
@@ -12,3 +12,30 @@ Si tu comentario es mayor a dos lieas van dentro de 3 comillas
 print("Hola mundo")
 print('Hola soy una linea de comando')
 
+variable1 = "Hola soy una cadena"
+print(len(variable1))
+print(variable1[2:5])
+print(variable1[2:])
+print(variable1[:4])
+
+#3. Variables
+
+#5Mike = o #no puedes crear variables con números al inicio
+
+
+x = "Miguel"
+x = 5.5
+x =4
+
+print (x)
+
+x = int(3)
+y = float(3)
+z = str(3)
+
+print(x, y, z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
