b42ba4cc5e4660a4c6412e28b15ac18f6756aadb
MiguelMartinez94 <122044737@upq.edu.mx>
2025-05-23 22:02:18 -0600

    Ejercicio 4
    Olvidé poner el try catch

--- practica3/Ejercicio4.py
+++ practica3/Ejercicio4.py
@@ -1,14 +1,18 @@
 
-contrasena = input("Ingresa una contraseña: ")
-    
-if len(contrasena) < 10:
-    print("Tu contraseña debe ser de al menos 10 caracteres")
-    pass
-elif not any(c.isdigit() for c in contrasena):
-    print("Tu contraseña debe tener un número.")
-    
-elif not any(c in "@!#$%&?¿+." for c in contrasena):
-    print("Tu contraseña debe tener un carácter especial")
+try:
+    contrasena = input("Ingresa una contraseña: ")
         
-else: 
-    print("Tu contraseña es válida!")
+    if len(contrasena) < 10:
+        print("Tu contraseña debe ser de al menos 10 caracteres")
+        pass
+    elif not any(c.isdigit() for c in contrasena):
+        print("Tu contraseña debe tener un número.")
+        
+    elif not any(c in "@!#$%&?¿+." for c in contrasena):
+        print("Tu contraseña debe tener un carácter especial")
+            
+    else: 
+        print("Tu contraseña es válida!")
+        
+except Exception:
+    print("Error!")
