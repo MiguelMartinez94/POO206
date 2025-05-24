cb98a931ed654dd31f29d195884cb660eb49aa96
MiguelMartinez94 <122044737@upq.edu.mx>
2025-05-23 23:23:23 -0600

    Ejercicio 3
    No me percaté que había cometido un error, no lo revisé dos veces, además agregué que se vieran las comparaciones de las palabras.

--- practica3/Ejercicoio3.py
+++ practica3/Ejercicoio3.py
@@ -2,14 +2,18 @@ try:
 
     palabra = str(input("Ingresa cualquier palabra: "))
 
-    if palabra.isalnum():
-        raise ValueError("Debes ingresar sólo cadenas de texto, no números")
-        pass
+    if any(p.isdigit() for p in palabra):
+        raise ValueError("Debes ingresar sólo cadenas de texto")
+        
     elif palabra != palabra[::-1]:
         print("No es palíndroma")
+        
+        print(f"{palabra} no es igual a {palabra[::-1]}")
 
     else:
         print("Es palindroma")
+        
+        print(f"WOW, la palabra que ingresaste: {palabra} se escibe igual al derecho y al revés, mira {palabra} vs {palabra[::-1]}")
         
 except ValueError:
     print("Debes ingresar sólo cadenas de texto, no números")
