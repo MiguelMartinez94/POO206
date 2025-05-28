dbe3eddf5147422816a02d70a4c80882e2ff54a1
MiguelMartinez94 <122044737@upq.edu.mx>
2025-05-27 21:11:41 -0600

    Ejercicio 2

---
+++ practica4/Ejercicio2.py
@@ -0,0 +1,10 @@
+numero = int(input("Ingresa un numero entero: "))
+
+if numero >0:
+            
+    for n in range(numero, -1,-1):
+        
+        print(f"{n},")
+        
+else:
+    print("No estás ingresnado un numero enteero")
