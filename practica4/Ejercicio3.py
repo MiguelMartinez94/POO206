ca75ccd5ab11144932e5265e00660a0902d6ce5e
MiguelMartinez94 <122044737@upq.edu.mx>
2025-05-28 09:46:03 -0600

    Ejercicio 3

--- practica4/Ejercicio3.py
+++ practica4/Ejercicio3.py
@@ -1,2 +1,23 @@
-frase = str(input("Ingresa cualquier frase: "))
 
+while True:
+    try:
+        
+        frase = str(input("Ingresa cualquier frase: "))
+        letra = str(input("Ingresa la letra que quieras buscar: "))
+
+        if any(f.isdigit() for f in frase):
+
+            print("No debes de ingresar números.")
+            
+        contador = 0
+
+        for c in frase:
+            if c == letra:
+                contador +=1
+
+
+        print(f"El la cantidad de veces que aparece {letra} en {frase} es: {contador}")
+        
+    except (ValueError, TypeError):
+        
+        print("Hemos encontrado un error en tu frase, vuelve a intentarlo :c")
