fcafcddcf40312cf38ffdde28d4cd721fa3ef33a
MiguelMartinez94 <122044737@upq.edu.mx>
2025-06-12 20:05:54 -0600

    Rutas en Flask

--- flaskProyect/app.py
+++ flaskProyect/app.py
@@ -3,9 +3,33 @@ from flask import Flask
 
 app = Flask (__name__)
 
+
+#ruta básica
 @app.route('/')
 def home():
     return 'Hola mundo FLASK'
+        
+#ruta con parámetros
+@app.route('/saludo/<nombre>')
+def saludo(nombre):
+    return '¡Hola, '+nombre+'!'
+
+        
+#ruta try-catch
+@app.errorhandler(404)
+def paginaNoE(e):
+    return 'Cuidado: Error de capa 8! :c', 404
+    
+#ruta doble
+@app.route('/usuario')
+@app.route('/usuaria')
+def dobleRoute():
+    return 'Hola soy el mismo recurso del servidor'
+
+#ruta POST
+@app.route('/usuario', methods = ['POST'])
+def formulario():
+    return 'Soy un formulario'
     
 if __name__ == '__main__':
     
