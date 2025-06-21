372f23670c6eddd9c315e5e3ceae8cad48dae3e5
MiguelMartinez94 <122044737@upq.edu.mx>
2025-06-20 18:52:35 -0600

    Practica 8 Vistas e Insert en Flask

--- flaskProyect/app.py
+++ flaskProyect/app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request, url_for, flash, redirect
 from flask_mysqldb import MySQL
 import MySQLdb
 
@@ -8,8 +8,9 @@ app = Flask (__name__)
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
 app.config['MYSQL_PASSWORD'] = "Kesadilla94"
-app.config['MYSQL_DB'] = "dbflask"
+app.config['MYSQL_DB'] = "db_albums"
 #app.config['MYSQL_PORT'] = 3306
+app.secret_key = 'mysecretkey'
 
 mysql = MySQL(app)
 
@@ -26,10 +27,6 @@ def DB_check():
     except MySQLdb.MySQLError as e:
         return jsonify({'status':'error', 'message':str(e)}), 500  
         
-#ruta con parámetros
-@app.route('/saludo/<nombre>')
-def saludo(nombre):
-    return '¡Hola, '+nombre+'!'
 
         
 #ruta try-catch
@@ -37,21 +34,65 @@ def saludo(nombre):
 def paginaNoE(e):
     return 'Cuidado: Error de capa 8! :c', 404
     
-#ruta doble
-@app.route('/usuario')
-@app.route('/usuaria')
-def dobleRoute():
-    return 'Hola soy el mismo recurso del servidor'
-
-#ruta POST
-@app.route('/usuario', methods = ['POST'])
-def formulario():
-    return 'Soy un formulario'
 
 @app.errorhandler(405)
 def metodoNoPermitido(e):
     return 'Revisa el meotodo de envio de tu ruta (GET o POST)', 405
 
+#Ruta de inicio
+@app.route('/')
+def home():
+    return render_template('formulario.html')
+
+#Ruta de consulta
+@app.route('/consulta')
+def consulta():
+    return render_template('consulta.html')
+
+#Ruta para el insert
+@app.route('/guardarAlbum', methods = ['POST'])
+def guardar():
+    
+    #lista de errores
+    errores = {}
+    
+    #Obtener los datos a insertar
+    titulo = request.form.get('txtTitulo', '').strip()
+    artista = request.form.get('txtArtista', '').strip()
+    anio = request.form.get('txtAnio', '').strip()
+    
+    if not titulo:
+        errores['txtTitulo'] = 'Nombre del álbum obligatorio'
+        
+    if not artista:
+        errores['txtArtista'] = 'Nombre del artista obligatorio'
+        
+    if not anio:
+        errores['txtAnio'] = 'Año del álbum obligatorio'
+        
+    elif not anio.isdigit() or int(anio) < 1800 or int(anio) > 2050:
+        errores['txtAnio'] = 'Ingresa un año válido'
+        
+    if not errores:
+    #Intentamos ejecutar el INSERT
+    
+        try:
+            cursor = mysql.connection.cursor()
+            cursor.execute('insert into albums(album, artista, anio) values(%s, %s, %s)', (titulo, artista, anio))
+            mysql.connection.commit()
+            flash('Album se guardó en BD')
+            return redirect(url_for('home'))
+            
+        except Exception as e:
+            mysql.connection.rollback()
+            flash( f'Algo falló: {str(e)}')
+            return redirect(url_for('home'))
+            
+        finally:
+            
+            cursor.close()
+    
+    return render_template('formulario.html', errores = errores)
     
 if __name__ == '__main__':
     
